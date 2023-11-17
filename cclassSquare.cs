cefde9b71bbdd41d4859aebb0fed3ee78a687b8b
Suhagan Mostahid <suhaganmostahid@yahoo.com>
2023-11-17 07:01:33 +0100

    Creating child class 'cclassSquare'

---
+++ cclassSquare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Polymorphism_in_OOP
+{
+	public class cclassSquare: pclassGeometry
+	{
+        /*  special property of square is all arms are of same length. 
+         *  so, created one special property: squareSide.
+        */
+
+        public double squareSideA { get; set; }
+
+
+        //constructor to creat object
+        public cclassSquare(double sideArmA)
+        {
+            squareSideA = sideArmA;
+        }
+
+        // method to calculate area
+        public override double Area()
+        {
+            double squareArea;
+            return squareArea = squareSideA * squareSideA;
+        }
+    }
+}
+
