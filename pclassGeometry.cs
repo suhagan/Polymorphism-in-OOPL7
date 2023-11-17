b1b7a162bd058c9485d9a48edbddb30d6c1b0710
Suhagan Mostahid <suhaganmostahid@yahoo.com>
2023-11-17 06:50:08 +0100

    creating parent class 'pclassGeometry'

---
+++ pclassGeometry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Polymorphism_in_OOP
+{
+	public class pclassGeometry
+	{
+        public virtual double Area()
+        /*In OOP, a child class can override a method from parent class
+         * when that class is marked as 'virtual' in the parent class.*/
+        {
+
+            return 0.0;
+
+        }
+    }
+}
+
