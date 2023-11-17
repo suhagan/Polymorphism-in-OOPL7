dfed12b4628dd745fa4720e379ff2c8e31e36ff5
Suhagan Mostahid <suhaganmostahid@yahoo.com>
2023-11-17 06:56:22 +0100

    creating child class 'cclassRectangle'

---
+++ cclassRectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Polymorphism_in_OOP
+{
+	public class cclassRectangle:pclassGeometry
+	{
+        
+            // properties
+            public double reclenGth { get; set; }
+            public double recwiDth { get; set; }
+
+            // constructor to
+            public cclassRectangle(double lenGth, double wiDth)
+            {
+                reclenGth = lenGth;
+                recwiDth = wiDth;
+            }
+
+            // method to calculate area
+            public override double Area()
+            {
+                double rectArea;
+                return rectArea = reclenGth * recwiDth;
+            }
+        
+    }
+}
+
