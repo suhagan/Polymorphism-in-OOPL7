ba2c57ce7ae139870f8825dc498050831d46b5bf
Suhagan Mostahid <suhaganmostahid@yahoo.com>
2023-11-20 06:42:22 +0100

    child class Circle and Ellipse created

---
+++ cclassCircle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Polymorphism_in_OOP
+{
+	public class cclassCircle:pclassGeometry
+	{
+        // properties for circle
+        public double circleRadius { get; set; }
+
+        // constructor circle
+        public cclassCircle(double raDius)
+        {
+            circleRadius = raDius;
+        }
+
+        // method to calculate area of the circle
+        public override double Area()
+        {
+            return Math.PI * circleRadius * circleRadius;
+        }
+    }
+}
+
---
+++ cclassEllipse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Polymorphism_in_OOP
+{
+	public class cclassEllipse:pclassGeometry
+	{
+        // properties for ellipse
+        public double majorAxis { get; set; }
+        public double minorAxis { get; set; }
+
+
+        // constructor
+        public cclassEllipse(double majorAx, double minorAx)
+        {
+            majorAxis = majorAx;
+            minorAxis = minorAx;
+        }
+
+
+        // method to calculate area of the ellipse
+        public override double Area()
+        {
+            return Math.PI * majorAxis * minorAxis;
+        }
+    }
+}
+
